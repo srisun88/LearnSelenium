45e5330ec90f61421bf974f850386ee97a6949d3
srisun88 <srisun88@gmail.com>
2025-11-09 07:24:26 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float a =  91.0f;
+		String s = "Google";
+		Boolean Beta = false;
+		int releaseYear = 2008;
+		char c = 'C';
+ System.out.println("Printing float: "+a);
+ System.out.println("Printing string: "+s);
+ System.out.println("Printing Boolean: "+Beta);
+ System.out.println("Printing int: "+releaseYear);
+ System.out.println("Printing char: "+c);
+	}
+
+}
